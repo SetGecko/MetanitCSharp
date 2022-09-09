b6f7df972594c72e8b87534135aa7dc7ad2eed33
Алексей Ипполитов <setgecko@gmail.com>
2022-09-10 01:18:06 +0300

    Добавил материалы по Главе 1 и начало поглаве 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("-----");
+
+Console.Write("Введите своё имя: ");
+string? name = Console.ReadLine();
+Console.WriteLine($"Привет {name}");
+//string? - значит что переменная может быть null
+
+Console.ReadKey();
