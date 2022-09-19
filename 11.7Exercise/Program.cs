923b699f75fea70bee55d4bb9226940f16b2f6be
Алексей Ипполитов <setgecko@gmail.com>
2022-09-19 05:41:41 +0300

    Пройдена тема 11 Конструкции if...else Пройдена практика по теме 11 Создан проект для темы 12 Циклы

---
+++ 11.7Exercise/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Упражнение 7
+Измените предыдущую программу. 
+Пусть пользователь кроме номера операции вводит два числа, 
+и в зависимости от номера операции с введенными числами выполняются определенные действия 
+(например, при вводе числа 3 числа умножаются). 
+Результа операции выводиться на консоль.
+ */
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите номер операции: \n1.Сложение \n2.Вычитание \n3.Умножение \n");
+int operation = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+
+switch (operation)
+{
+    case 1:
+        result = num1 + num2;
+        Console.WriteLine($"Результатом сложения {num1} и {num2} равно {result}");
+        break;
+    case 2:
+        result = num1 - num2;
+        Console.WriteLine($"Результатом разности из {num1} и {num2} равно {result}");
+        break;
+    case 3:
+        result = num1 * num2;
+        Console.WriteLine($"Результатом произведения из {num1} и {num2} равно {result}");
+        break;
+    default:
+        Console.WriteLine("Операция не определена");
+        break;
+}
--- 11.ConstructIfElse/Program.cs
+++ 11.ConstructIfElse/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿Console.WriteLine("\tСравнения  if...else if...else");
+Console.WriteLine("-----");
+Console.Write("Введите первое число: ");
+var num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+var num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"Число {num1} больше числа {num2}"); 
+}
+else if (num1 < num2)
+{
+    Console.WriteLine($"Число {num1} меньше числа {num2}");
+}
+else
+{
+    Console.WriteLine("Оба введённые числа равны");
+}
+
+
+Console.Write("Введите имя котейки:");
+string? petName = Console.ReadLine();
+
+if (petName == "Жужа" || petName == "жужа")
+{
+    Console.WriteLine("У вас самая сладкая котейка!");
+}
+else
+{
+    Console.WriteLine($"Вашу котейку зовут {petName}");
+}
+
+Console.WriteLine("\n-----");
+Console.WriteLine("\tТернарные операции");
+Console.WriteLine("-----");
+Console.WriteLine("\n[первый операнд - условие] ? [второй операнд] : [третий операнд]");
+int x = 3;
+int y = 2;
+int z = x < y ? (x + y) : (x - y);
+Console.WriteLine("x = 3\ny = 2\nz = x < y ? (x + y) : (x - y)");
+Console.WriteLine(z);
