9d71cd7afa96e02bb5fb4019621b5dc3d89a8564
Алексей Ипполитов <setgecko@gmail.com>
2022-09-22 20:56:27 +0300

    Выполнено упражнение 2.12.1 и 2.12.2

---
+++ 12.1Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Введите сумму вклада: ");
+decimal summ = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Введите количество месяцев для хранения вклада: ");
+int monthAmount = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < (monthAmount + 1); i++)
+{
+    summ = summ + (summ * 0.07M);
+    Console.WriteLine($"В {i} месяц сумма по вкладам с процентами равна {summ}");
+}
---
+++ 12.2Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите сумму вклада: ");
+decimal summ = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Введите количество месяцев для хранения вклада: ");
+int monthAmount = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i < (monthAmount + 1))
+{
+    summ = summ + (summ * 0.07M);
+    Console.WriteLine($"В {i} месяц сумма по вкладам с процентами равна {summ}");
+    i++;
+}
