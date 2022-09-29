2de9a10d262c347cf6771e08cb6efedfe12c89c5
Алексей Ипполитов <setgecko@gmail.com>
2022-09-29 16:42:18 +0300

    Пройдена практика по 2.17 Возвращение значения и оператор return

--- 17.ReturningValueAndReturnStatement/Program.cs
+++ 17.ReturningValueAndReturnStatement/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿string GetMessage()
+{
+    return "bububu";
+}
+
+string message = GetMessage();
+Console.WriteLine(message);
+
+void PrintMessage(string message)
+{
+    Console.WriteLine(message);
+}
+
+PrintMessage(GetMessage());
+
+int Sum (int x, int y)
+{
+    return x + y;
+}
+
+int result = Sum(5, 6);
+Console.WriteLine(result);
+
+Console.WriteLine(Sum(10, 15));
+
+void PrintPerson(string name, int age)
+{
+    if (age > 120 || age < 1)
+    {
+        Console.WriteLine("Недопустимый возраст");
+        return;
+    }
+    Console.WriteLine($"Имя {name} Возраст {age}");
+}
+
+PrintPerson("Жужа", 4);
+PrintPerson("Том", 1234);
