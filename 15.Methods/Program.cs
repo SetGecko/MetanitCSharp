d307bc899de44d27a7dbe382c01295d5fc66ba62
Алексей Ипполитов <setgecko@gmail.com>
2022-09-29 13:26:09 +0300

    Пройдена тема 2.15 Методы Создан проект для темы 2.16 параметры методов

--- 15.Methods/Program.cs
+++ 15.Methods/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿void SayHelloRu()
+{
+    Console.WriteLine("Привет");
+}
+
+void SayHelloEn()
+{
+    Console.WriteLine("Hello");
+}
+
+void SayHelloFr()
+{
+    Console.WriteLine("Salut");
+}
+
+string language = "fr";
+
+switch (language)
+{
+    case "en":
+        SayHelloEn();
+        break;
+    case "ru":
+        SayHelloRu();
+        break;
+    case "fr":
+        SayHelloFr();
+        break;
+}
+
+// Сокращённая запись методов
+
+void Bububu() => Console.WriteLine("Bububu");
+
+Bububu();
