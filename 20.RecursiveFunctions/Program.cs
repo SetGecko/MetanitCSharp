70e63dbd2f8bd22139205b2ff5b216e5c5024aae
Алексей Ипполитов <setgecko@gmail.com>
2022-10-04 19:28:50 +0300

    Пройдена тема 2.20 Рекурсивные функции

--- 20.RecursiveFunctions/Program.cs
+++ 20.RecursiveFunctions/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿// Нахождение факториала рекурсивной функцией
+int Factorial(int n)
+{
+    if (n == 0)
+        return 1;
+    else
+        return n * Factorial(n - 1);
+}
+int x = 5;
+Console.WriteLine(Factorial(x));
+Console.WriteLine(Factorial(4));
+Console.WriteLine(Factorial(7));
+Console.WriteLine("--------------------------");
+
+int Fibonachi(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    else
+        return Fibonachi(n - 1) + Fibonachi(n - 2);
+}
+
+Console.WriteLine(Fibonachi(4));
+Console.WriteLine(Fibonachi(6));
+Console.WriteLine(Fibonachi(8));
