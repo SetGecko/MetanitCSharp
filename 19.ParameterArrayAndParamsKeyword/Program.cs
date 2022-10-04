a5d13f3a5396aa349dd571399d2289b80c7ff38b
Алексей Ипполитов <setgecko@gmail.com>
2022-10-04 18:48:56 +0300

    Пройдена тема 2.19 Массив параметров и ключевое слово params

--- 19.ParameterArrayAndParamsKeyword/Program.cs
+++ 19.ParameterArrayAndParamsKeyword/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿void Addition (params int[] numbers)
+{
+    int result = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        result = result + numbers[i];
+    }
+    Console.WriteLine (result);
+}
+Addition(new int[] { 1, 2, 3, 4 });
+Addition(1, 1, 1, 2);
+Addition();
+
+void Sum(int initialValue, params int[] numbers)
+{
+    int result = initialValue;
+    foreach (var n in numbers)
+    {
+        result += n;
+    }
+    Console.WriteLine(result);
+}
+
+int[] nums = { 1, 2, 3, 4, 5 };
+Sum(10, nums);  // число 10 - передается параметру initialValue
+Sum(1, 2, 3, 4);
+Sum(1, 2, 3);
+Sum(20);
+
