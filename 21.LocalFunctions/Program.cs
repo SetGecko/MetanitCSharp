4174350050f0372d7c477882c50697113040f050
Алексей Ипполитов <setgecko@gmail.com>
2022-10-10 06:13:57 +0300

    Код для сравнения сумм 2 массивов

--- 21.LocalFunctions/Program.cs
+++ 21.LocalFunctions/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿void Compare(int[] numbers1, int[] numbers2)
+{
+    int numbers1sum = 0;
+    int numbers2sum = 0;
+
+    foreach(int number in numbers1)
+    {
+        numbers1sum = numbers1sum + number;
+    }
+
+    foreach (int number in numbers2)
+    {
+        numbers2sum = numbers2sum + number;
+    }
+
+    if(numbers1sum > numbers2sum)
+    {
+        Console.WriteLine("Сумма чисел из массива numbers1 больше чем в массиве numbers2");
+    }
+    else
+    {
+        Console.WriteLine("Сумма чисел из массива numbers2 больше чем в массиве numbers1");
+    }
+}
+
+int[] numbers1 = { 1, 2, 3 };
+int[] numbers2 = { 1, 2, 3, 4, 5, 6 };
+
+Compare(numbers1, numbers2);
+
+
