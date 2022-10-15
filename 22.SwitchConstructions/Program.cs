87985571ca1bc50c7081294e64fa6ceb63a3c461
Алексей Ипполитов <setgecko@gmail.com>
2022-10-15 19:39:02 +0300

    Пройдена основная коснтрукция switch

--- 22.SwitchConstructions/Program.cs
+++ 22.SwitchConstructions/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.WriteLine("Введите имя: ");
+string name = Console.ReadLine();
+
+switch (name)
+{
+    case "Жужа":
+        Console.WriteLine("Даааа, этоо жужааа");
+        break;
+    case "Tom":
+        Console.WriteLine("Ваше имя Tom");
+        break;
+    case "Bob":
+        Console.WriteLine("Ваше имя Bob");
+        break;
+    default:
+        Console.WriteLine("неизвестное имя");
+        break;
+}
