152079e001964d61259d722791f3faf4c95f333d
Алексей Ипполитов <setgecko@gmail.com>
2022-10-15 19:53:37 +0300

    Пройдена практика по созданию метода со встроенным switch

--- 22.SwitchConstructions/Program.cs
+++ 22.SwitchConstructions/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите имя: ");
+﻿/*
+Console.WriteLine("Введите имя: ");
 string name = Console.ReadLine();
 
 switch (name)
@@ -16,3 +17,24 @@ switch (name)
         Console.WriteLine("неизвестное имя");
         break;
 }
+*/
+
+int DoOperation (int op, int a, int b)
+{
+    switch (op)
+    {
+        case 1: return a + b;
+        case 2: return a - b;
+        case 3: return a * b;
+        default: return 0;
+    }
+}
+
+int result1 = DoOperation(1, 2, 3); // 5
+Console.WriteLine(result1);
+
+int result2 = DoOperation(2, 6, 3); // 3
+Console.WriteLine (result2);
+
+int result3 = DoOperation(3, 1, 7); // 7
+Console.WriteLine (result3);
