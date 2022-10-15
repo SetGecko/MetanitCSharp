98efde801e45e467f5a5f2405f2d3d9fde73b61d
Алексей Ипполитов <setgecko@gmail.com>
2022-10-15 20:26:23 +0300

    Перечисление как хранение состояния

--- 23.Enums/Program.cs
+++ 23.Enums/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿DayTime now = DayTime.Evening;
+
+PrintMessage(now);                  // Добрый вечер
+PrintMessage(DayTime.Afternoon);    // Добрый день
+PrintMessage(DayTime.Night);        // Доброй ночи
+
+void PrintMessage(DayTime dayTime)
+{
+    switch (dayTime)
+    {
+        case DayTime.Morning:
+            Console.WriteLine("Доброе утро");
+            break;
+        case DayTime.Afternoon:
+            Console.WriteLine("Добрый день");
+            break;
+        case DayTime.Evening:
+            Console.WriteLine("Добрый вечер");
+            break;
+        case DayTime.Night:
+            Console.WriteLine("Доброй ночи");
+            break;
+    }
+}
+
+enum DayTime
+{
+    Morning,
+    Afternoon,
+    Evening,
+    Night
+}
+
+// Console.WriteLine();
+
+
+
+
+enum Operation
+{
+    Add,
+    Substract,
+    Multiply,
+    Divide
+}
