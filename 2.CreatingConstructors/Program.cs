c0a6fb986b7128999554ff88fc189cb8db8b43c5
Алексей Ипполитов <setgecko@gmail.com>
2022-10-17 07:39:50 +0300

    Пройдена практика 3.2 - Создание конструкторов

--- 2.CreatingConstructors/Program.cs
+++ 2.CreatingConstructors/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿Person tom = new Person();
+Person bob = new Person("Bob");
+Person sam = new Person("Sam", 25);
+
+tom.Print();
+bob.Print();
+sam.Print();
+
+class Person
+{
+    public string name;
+    public int age;
+    public Person() { name = "неизвестно"; age = 18; }
+    public Person(string n) { name = n; age = 18; }
+    public Person(string n, int a) { name = n; age = a; }
+
+    public void Print()
+    {
+        Console.WriteLine($"Имя {name}, возраст {age}");
+    }
+}
