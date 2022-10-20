2e0f3dd08eb2d5d6af4582dc43a2dca108f5069c
Алексей Ипполитов <setgecko@gmail.com>
2022-10-20 21:08:49 +0300

    Инициализация с помощью конструктора

--- 4.Structures/Program.cs
+++ 4.Structures/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Person tom = new Person();
+
+tom.name = "Tom";
+tom.Print();
+
+struct Person
+{
+    public string name;
+    public int age;
+
+    public void Print()
+    {
+        Console.WriteLine($"Имя: {name}, возраст: {age}");
+    }
+}
