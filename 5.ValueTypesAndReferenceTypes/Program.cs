09b41df3483c0e2dd1056e57bd171189464a08bb
Алексей Ипполитов <setgecko@gmail.com>
2022-10-26 23:21:05 +0300

    Практика по составным типам

--- 5.ValueTypesAndReferenceTypes/Program.cs
+++ 5.ValueTypesAndReferenceTypes/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿State state1 = new State();
+Country country1 = new Country();
+
+struct State
+{
+    public int x;
+    public int y;
+    public Country country;
+    public State()
+    {
+        x = 0;
+        y = 0;
+        country = new Country();
+    }
+}
+class Country
+{
+    public int x;
+    public int y;
+}
