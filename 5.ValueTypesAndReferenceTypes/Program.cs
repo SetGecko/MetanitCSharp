67533db5c0b334a27381eb3ef16b0a14b39c6fc5
Алексей Ипполитов <setgecko@gmail.com>
2022-10-26 23:23:07 +0300

    Практика по Объекты классов как параметры методов

--- 5.ValueTypesAndReferenceTypes/Program.cs
+++ 5.ValueTypesAndReferenceTypes/Program.cs
@@ -1,30 +1,22 @@
-﻿// Ссылочные типы внутри типов значений
+﻿// Объекты классов как параметры методов
 
-State state1 = new State();
-State state2 = new State();
+Person p = new Person { name = "Tom", age = 23 };
+ChangePerson(p);
 
-state2.country = new Country();
-state2.country.x = 5;
-state1 = state2;
-state2.country.x = 8; // теперь и state1.country.x=8, так как state1.country и state2.country
-                      // указывают на один объект в хипе
-Console.WriteLine(state1.country.x); // 8
-Console.WriteLine(state2.country.x); // 8
+Console.WriteLine(p.name); // Alice
+Console.WriteLine(p.age); // 23
 
-struct State
+void ChangePerson(Person person)
 {
-    public int x;
-    public int y;
-    public Country country;
-    public State()
-    {
-        x = 0;
-        y = 0;
-        country = new Country(); // выделение памяти для объекта Country
-    }
+    // сработает
+    person.name = "Alice";
+    // сработает только в рамках данного метода
+    person = new Person { name = "Bill", age = 45 };
+    Console.WriteLine(person.name); // Bill
 }
-class Country
+
+class Person
 {
-    public int x;
-    public int y;
+    public string name = "";
+    public int age;
 }
