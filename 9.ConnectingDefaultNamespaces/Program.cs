c6183cc671cbd32af6c0a53cc55e63a225cc8850
Алексей Ипполитов <setgecko@gmail.com>
2022-10-31 09:39:37 +0300

    Пройдена практика по теме 3.9 Подключение пространств имен по умолчанию

--- 9.ConnectingDefaultNamespaces/Program.cs
+++ 9.ConnectingDefaultNamespaces/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System.Net.NetworkInformation;
+
+System.Console.WriteLine("Привет!");
+
+/*
+  < ItemGroup >
+    < Using Include = "System" />
+    < Using Include = "System.Threading.Tasks" />
+или
+    < Using Remove = "System" />
+    < Using Remove = "System.Threading.Tasks" />
+  </ ItemGroup >
+*/
