e013be560d71405ad5c5a9591bf25038f6f1818e
Алексей Ипполитов <setgecko@gmail.com>
2022-11-08 18:45:00 +0300

    Установка вычисляемых свойств

--- 12.Properties/Program.cs
+++ 12.Properties/Program.cs
@@ -1,25 +1,16 @@
-﻿Person person = new Person();
-
-Console.WriteLine(person.Age);  // 1
-// изменяем значение свойства
-person.Age = 37;
-Console.WriteLine(person.Age);  // 37
-// пробуем передать недопустимое значение
-person.Age = -23;               // Возраст должен быть в диапазоне от 1 до 120
-Console.WriteLine(person.Age);  // 37 - возраст не изменился
-
+﻿Person tom = new("Tom", "Smith");
+Console.WriteLine(tom.Name);    // Tom Smith
 class Person
 {
-    int age = 1;
-    public int Age
+    string firstName;
+    string lastName;
+    public string Name
     {
-        set
-        {
-            if (value < 1 || value > 120)
-                Console.WriteLine("Возраст должен быть в диапазоне от 1 до 120");
-            else
-                age = value;
-        }
-        get { return age; }
-    } 
+        get { return $"{firstName} {lastName}"; }
+    }
+    public Person(string firstName, string lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
