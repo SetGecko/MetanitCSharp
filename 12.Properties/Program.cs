06d54495355dd0d042eebd69355b584c0265f60d
Алексей Ипполитов <setgecko@gmail.com>
2022-11-08 18:32:05 +0300

    Создание гетеров и сетеров без логики

--- 12.Properties/Program.cs
+++ 12.Properties/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Person person = new Person();
+
+// Устанавливаем свойство - срабатывает блок Set
+// значение "Tom" и есть передаваемое в свойство value
+person.Name = "Tom";
+
+// Получаем значение свойства и присваиваем его переменной - срабатывает блок Get
+string personName = person.Name;
+Console.WriteLine(personName);  // Tom
+
+class Person
+{
+    private string name = "Undefined";
+
+    public string Name
+    {
+        get { return name; } // возвращаем значение свойства
+        set { name = value; } //устанавливаем новое значение свойства
+    }
+}
