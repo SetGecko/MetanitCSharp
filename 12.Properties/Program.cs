345fd0593f6730df46c8edcd969289fce8b58f3f
Алексей Ипполитов <setgecko@gmail.com>
2022-11-08 18:48:43 +0300

    автосвойства со значениями по умолчанию

--- 12.Properties/Program.cs
+++ 12.Properties/Program.cs
@@ -1,16 +1,10 @@
-﻿Person tom = new("Tom");
+﻿Person tom = new();
 
-// Ошибка - set объявлен с модификатором private
-//tom.Name = "Bob";
 Console.WriteLine(tom.Name);    // Tom
+Console.WriteLine(tom.Age);    // 37
+
 class Person
 {
-    string name = "";
-    public string Name
-    {
-        get { return name; }
-
-        private set { name = value; }
-    }
-    public Person(string name) => Name = name;
+    public string Name { get; set; } = "Tom";
+    public int Age { get; set; } = 37;
 }
