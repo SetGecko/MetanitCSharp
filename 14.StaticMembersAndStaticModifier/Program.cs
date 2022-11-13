fa52f9bb231ab99c44c54bc59b0cc64009220a32
Алексей Ипполитов <setgecko@gmail.com>
2022-11-13 14:33:50 +0300

    Практика 3.14 по статическому полю retirementAge

--- 14.StaticMembersAndStaticModifier/Program.cs
+++ 14.StaticMembersAndStaticModifier/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿Person Tom = new Person(69);
+Tom.CheckAge();
+
+Person Bob = new Person(35);
+Bob.CheckAge();
+
+class Person
+{
+    int age;
+    public static int retirementAge = 65;
+    public Person (int age)
+    {
+        this.age = age;
+    }
+
+    public void CheckAge()
+    {
+        if (age > retirementAge)
+            Console.WriteLine("Уже на пенсии");
+        else
+            Console.WriteLine($"Сколько лет осталось до пенсии: {retirementAge - age}");
+    }
+}
