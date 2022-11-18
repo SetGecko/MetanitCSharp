2a54e5be277efd8266d3320fab0605edce7eba8d
Алексей Ипполитов <setgecko@gmail.com>
2022-11-18 17:53:42 +0300

    Практика по 3.16 Константы классы

--- 16.ConstantsFieldsAndStructursForReading/Program.cs
+++ 16.ConstantsFieldsAndStructursForReading/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Person tom = new Person();
+tom.name = "Tom";
+tom.Print();
+
+Console.WriteLine(Person.type);
+
+
+
+class Person
+{
+    public const string type = "Person";
+    public string name = "Undefined";
+    public void Print()
+    {
+        Console.WriteLine($"{type}: {name}");
+    }
+}
