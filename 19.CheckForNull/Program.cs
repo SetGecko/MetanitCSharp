e4ac8f0fe2abccef8042a43fe1d854bec1523792
Алексей Ипполитов <setgecko@gmail.com>
2022-11-22 20:39:24 +0300

    Практика 3.19 Оператор ?? и ??=

--- 19.CheckForNull/Program.cs
+++ 19.CheckForNull/Program.cs
@@ -1,14 +1,19 @@
-﻿PrintUpper(null);
-PrintUpper("Жужа");
+﻿string? text = null;
+string name = text ?? "Tom";  // равно Tom, так как text равен null
+Console.WriteLine(name);    // Tom
 
-void PrintUpper(string? text)
-{
-    if (text is string)
-    {
-        Console.WriteLine(text.ToUpper());
-    }
-    else
-    {
-        Console.WriteLine("NULL");
-    }
-}
+int? id = 200;
+int personid = id ?? 1; // равно 200, так как id не равен null
+Console.WriteLine(personid);    // 200
+
+string? text2 = null;
+text2 ??= "Sam";
+// аналогично
+// text = text ?? "Sam";
+Console.WriteLine(text2);    // Sam
+
+int? id1 = 100;
+id1 ??= 1;
+// аналогично
+//id = id ?? 1;
+Console.WriteLine(id1); // 100
