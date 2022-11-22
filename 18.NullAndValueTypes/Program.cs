80b0efde3fb015ba98086508503e9e644acb5c51
Алексей Ипполитов <setgecko@gmail.com>
2022-11-22 19:31:46 +0300

    Практика по 3.18 создание переменной типа int?

--- 18.NullAndValueTypes/Program.cs
+++ 18.NullAndValueTypes/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int? val = null;
+IsNull(val);    // null
+val = 22;
+IsNull(val);    // 22
+
+void IsNull(int? obj)
+{
+    if (obj == null) Console.WriteLine("null");
+    else Console.WriteLine(obj);
+}
