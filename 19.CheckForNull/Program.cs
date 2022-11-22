1407c26002eff49e798330900a0bc379865a7465
Алексей Ипполитов <setgecko@gmail.com>
2022-11-22 20:44:52 +0300

    Практика 3.19 Оператор условного null

--- 19.CheckForNull/Program.cs
+++ 19.CheckForNull/Program.cs
@@ -1,19 +1,25 @@
-﻿string? text = null;
-string name = text ?? "Tom";  // равно Tom, так как text равен null
-Console.WriteLine(name);    // Tom
+﻿using System;
 
-int? id = 200;
-int personid = id ?? 1; // равно 200, так как id не равен null
-Console.WriteLine(personid);    // 200
+void PrintWebSite(Person? person)
+{
+    Console.WriteLine(person?.Company?.WebSite?.ToUpper());
+}
 
-string? text2 = null;
-text2 ??= "Sam";
-// аналогично
-// text = text ?? "Sam";
-Console.WriteLine(text2);    // Sam
+// или
 
-int? id1 = 100;
-id1 ??= 1;
-// аналогично
-//id = id ?? 1;
-Console.WriteLine(id1); // 100
+void PrintWebSite(Person? person)
+{
+    if (person != null && person.Company != null && person.Company.WebSite != null)
+    {
+        Console.WriteLine(person.Company.WebSite.ToUpper());
+    }
+}
+
+class Person
+{
+    public Company? Company { get; set; }   // место работы
+}
+class Company
+{
+    public string? WebSite { get; set; }    // веб-сайт компании
+}
