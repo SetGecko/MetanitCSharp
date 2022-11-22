d669dfb00ff958e1a443bacf7f4d28a5050899fc
Алексей Ипполитов <setgecko@gmail.com>
2022-11-22 19:43:15 +0300

    Практика по 3.18  Свойства Value и HasValue и метод GetValueOrDefault

--- 18.NullAndValueTypes/Program.cs
+++ 18.NullAndValueTypes/Program.cs
@@ -1,10 +1,20 @@
-﻿int? val = null;
-IsNull(val);    // null
-val = 22;
-IsNull(val);    // 22
+﻿int? number = null; // если значения нет, метод возвращает значение по умолчанию
+Console.WriteLine(number.GetValueOrDefault());      // 0  - значение по умолчанию для числовых типов
+Console.WriteLine(number.GetValueOrDefault(10));    // 10
 
-void IsNull(int? obj)
+number = 15;    // если значение задано, оно возвращается методом
+Console.WriteLine(number.GetValueOrDefault());    // 15
+Console.WriteLine(number.GetValueOrDefault(10));  // 15
+
+
+void PrintNullable(int? number)
 {
-    if (obj == null) Console.WriteLine("null");
-    else Console.WriteLine(obj);
+    if(number.HasValue)
+    {
+        Console.WriteLine(number.Value);
+    }
+    else
+    {
+        Console.WriteLine("Параметр равен null");
+    }
 }
