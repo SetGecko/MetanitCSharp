352b9261a889ab59201f663aea086d7df14d3c7e
Алексей Ипполитов <setgecko@gmail.com>
2022-11-22 19:52:41 +0300

    Практика по 3.18 Преобразование значимых nullable-типов

--- 18.NullAndValueTypes/Program.cs
+++ 18.NullAndValueTypes/Program.cs
@@ -1,20 +1,32 @@
-﻿int? number = null; // если значения нет, метод возвращает значение по умолчанию
-Console.WriteLine(number.GetValueOrDefault());      // 0  - значение по умолчанию для числовых типов
-Console.WriteLine(number.GetValueOrDefault(10));    // 10
-
-number = 15;    // если значение задано, оно возвращается методом
-Console.WriteLine(number.GetValueOrDefault());    // 15
-Console.WriteLine(number.GetValueOrDefault(10));  // 15
-
-
-void PrintNullable(int? number)
+﻿// явное преобразование от T? к T
+int? x1 = null;
+if(x1.HasValue)
 {
-    if(number.HasValue)
-    {
-        Console.WriteLine(number.Value);
-    }
-    else
-    {
-        Console.WriteLine("Параметр равен null");
-    }
+    int x2 = (int)x1;
+    Console.WriteLine(x2);
+}
+
+// неявное преобразование от T к T?
+int x3 = 4;
+int? x4 = x3;
+Console.WriteLine(x4);
+
+// неявные расширяющие преобразования от V к T?
+
+int x5 = 4;
+long? x6 = x5;
+Console.WriteLine(x6);
+
+// явные сужающие преобразования от V к T?
+long x7 = 4;
+int? x8 = (int)x7; // это аналогично:
+
+long? x9 = 4;
+int? x10 = (int)x9;
+
+// явные сужающие преобразования от V? к T
+long? x11 = null;
+if(x11.HasValue)
+{
+    int x12 = (int)x11;
 }
