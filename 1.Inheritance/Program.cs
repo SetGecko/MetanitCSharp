a0ed5b29c85878941e603ea77b9376a61b148eba
Алексей Ипполитов <setgecko@gmail.com>
2022-11-24 15:02:46 +0300

    Практика по 4.1 Наследование

--- 1.Inheritance/Program.cs
+++ 1.Inheritance/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿Person person = new Person { Name = "Tom" };
+person.Print();   // Tom
+person = new Employee { Name = "Sam" };
+person.Print();   // Sam
+
+class Person
+{
+    private string _name = "";
+
+    public string Name
+    {
+        get { return _name;}
+        set { _name = value; }
+    }
+    
+    public void Print()
+    {
+        Console.WriteLine(Name);
+    }
+}
+
+class Employee : Person
+{
+
+}
