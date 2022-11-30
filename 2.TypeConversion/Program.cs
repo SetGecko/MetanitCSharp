2c6b0aa85f8626662094c910d02f1c16e42435c7
Алексей Ипполитов <setgecko@gmail.com>
2022-11-30 17:55:57 +0300

    Практика 4.2 Создание наследования Object -> Person -> Employee|Client

--- 2.TypeConversion/Program.cs
+++ 2.TypeConversion/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿class Person
+{
+    public string Name { get; set; }
+    public Person(string name)
+    {
+        Name = name;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Person {Name}");
+    }
+}
+
+class Employee : Person
+{
+    public string Company { get; set; }
+    public Employee(string name, string company) : base(name)
+    {
+        Company = company;
+    }
+}
+
+class Client : Person
+{
+    public string Bank { get; set; }
+    public Client(string name, string bank) : base(name)
+    {
+        Bank = bank;
+    }
+}
