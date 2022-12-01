e1bd4a5431bbb053ddf7adcb6057e8ab42157716
Алексей Ипполитов <setgecko@gmail.com>
2022-12-01 10:52:08 +0300

    Практика 4.3 Переопределение свойств

--- 3.VirtualMethodsAndproperties/Program.cs
+++ 3.VirtualMethodsAndproperties/Program.cs
@@ -1,34 +1,40 @@
 ﻿Person bob = new Person("Bob");
-bob.Print(); // вызов метода Print из класса Person
+Console.WriteLine(bob.Age); // 1
 
 Employee tom = new Employee("Tom", "Microsoft");
-tom.Print(); // вызов метода Print из класса Person
+Console.WriteLine(tom.Age); // 18
+tom.Age = 22;
+Console.WriteLine(tom.Age); // 22
+tom.Age = 12;
+Console.WriteLine(tom.Age); // 22
 
 class Person
 {
+    int age = 1;
+    public virtual int Age
+    {
+        get => age;
+        set { if (value > 0 && value < 110) age = value; }
+    }
     public string Name { get; set; }
     public Person(string name)
     {
         Name = name;
     }
-    public virtual void Print()
-    {
-        Console.WriteLine(Name);
-    }
+    public virtual void Print() => Console.WriteLine(Name);
 }
 class Employee : Person
 {
+    public override int Age
+    {
+        get => base.Age;
+        set { if (value > 17 && value < 110) base.Age = value; }
+    }
     public string Company { get; set; }
-
     public Employee(string name, string company)
-            : base(name)
+        : base(name)
     {
         Company = company;
-    }
-
-    public override void Print()
-    {
-        base.Print();
-        Console.WriteLine($"работает в {Company}");
+        base.Age = 18; // возраст для работников по умолчанию
     }
 }
