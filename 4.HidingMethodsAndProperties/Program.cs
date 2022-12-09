a9cb3a942507f76418707db005973bf65ea9f026
Алексей Ипполитов <setgecko@gmail.com>
2022-12-09 04:03:24 +0300

    Практика по теме 4.4 создание скрытого члена класса Print

--- 4.HidingMethodsAndProperties/Program.cs
+++ 4.HidingMethodsAndProperties/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿class Person
+{
+    public string Name { get; set; }
+    public Person(string name)
+    {
+        Name = name;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Name: {Name}");
+    }
+}
+class Employee : Person
+{
+    public string Company { get; set; }
+    public Employee(string name, string company)
+                : base(name)
+    {
+        Company = company;
+    }
+    public new void Print()
+    {
+        Console.WriteLine($"Name: {Name}   Company: {Company}");
+    }
+}
