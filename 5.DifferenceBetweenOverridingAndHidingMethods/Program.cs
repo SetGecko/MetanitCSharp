eb4dfc618a758f50fac2c39eddd5e99a9a5f150e
Алексей Ипполитов <setgecko@gmail.com>
2022-12-09 04:39:29 +0300

    Практика по теме 4.5 Переопределение методов

--- 5.DifferenceBetweenOverridingAndHidingMethods/Program.cs
+++ 5.DifferenceBetweenOverridingAndHidingMethods/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿Person tom = new Employee("Tom", "Microsoft");
+tom.Print();        // Tom работает в Microsoft
+
+class Person
+{
+    public string Name { get; set; }
+    public Person(string name)
+    {
+        Name = name;
+    }
+    public virtual void Print()
+    {
+        Console.WriteLine(Name);
+    }
+}
+class Employee : Person
+{
+    public string Company { get; set; }
+    public Employee(string name, string company)
+        : base(name)
+    {
+        Company = company;
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine($"{Name} работает в {Company}");
+    }
+}
