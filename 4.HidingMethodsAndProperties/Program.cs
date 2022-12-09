f0d6b0df568042cb1952a8dfc022093208406aa8
Алексей Ипполитов <setgecko@gmail.com>
2022-12-09 04:08:44 +0300

    Практика по теме 4.4 создание скрытого члена классаи получение данных из базового класса

--- 4.HidingMethodsAndProperties/Program.cs
+++ 4.HidingMethodsAndProperties/Program.cs
@@ -1,25 +1,30 @@
-﻿class Person
+﻿Person bob = new Person("Bob");
+Console.WriteLine(bob.Name);    // Bob
+
+Employee tom = new Employee("Tom", "Microsoft");
+Console.WriteLine(tom.Name);    // Mr./Ms. Tom
+
+class Person
 {
     public string Name { get; set; }
     public Person(string name)
     {
         Name = name;
     }
-    public void Print()
-    {
-        Console.WriteLine($"Name: {Name}");
-    }
 }
 class Employee : Person
 {
+    // скрываем свойство Name базового класса
+    public new string Name
+    {
+        get => $"Mr./Ms. {base.Name}";
+        set => base.Name = value;
+
+    }
     public string Company { get; set; }
     public Employee(string name, string company)
                 : base(name)
     {
         Company = company;
-    }
-    public new void Print()
-    {
-        Console.WriteLine($"Name: {Name}   Company: {Company}");
     }
 }
