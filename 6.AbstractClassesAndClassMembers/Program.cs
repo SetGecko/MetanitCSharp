0c8cb09d77d98e28c59dcc6d0812ed43d456b33c
Алексей Ипполитов <setgecko@gmail.com>
2022-12-10 17:10:45 +0300

    Практика по теме 4.6 Пример абстрактного класса

--- 6.AbstractClassesAndClassMembers/Program.cs
+++ 6.AbstractClassesAndClassMembers/Program.cs
@@ -1,16 +1,39 @@
-﻿Transport tesla = new Auto();
-tesla.Move();           // легковая машина едет
-abstract class Transport
-{
-    public abstract void Move();
-}
-// класс машины
-abstract class Car : Transport { }
+﻿var rectanle = new Rectangle { Width = 20, Height = 20 };
+var circle = new Circle { Radius = 200 };
+PrintShape(rectanle); // Perimeter: 80   Area: 400
+PrintShape(circle); // Perimeter: 1256  Area: 125600
 
-class Auto : Car
+void PrintShape(Shape shape)
 {
-    public override void Move()
-    {
-        Console.WriteLine("легковая машина едет");
-    }
+    Console.WriteLine($"Perimeter: {shape.GetPerimeter()}  Area: {shape.GetArea()}");
+}
+
+// абстрактный класс фигуры
+abstract class Shape
+{
+    // абстрактный метод для получения периметра
+    public abstract double GetPerimeter();
+    // абстрактный метод для получения площади
+    public abstract double GetArea();
+}
+// производный класс прямоугольника
+class Rectangle : Shape
+{
+    public float Width { get; set; }
+    public float Height { get; set; }
+
+    // переопределение получения периметра
+    public override double GetPerimeter() => Width * 2 + Height * 2;
+    // переопрелеление получения площади
+    public override double GetArea() => Width * Height;
+}
+// производный класс окружности
+class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    // переопределение получения периметра
+    public override double GetPerimeter() => Radius * 2 * 3.14;
+    // переопрелеление получения площади
+    public override double GetArea() => Radius * Radius * 3.14;
 }
